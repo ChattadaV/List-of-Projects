56768970f9907459e703e70568a88713713a8af7
ChattadaV <71411487+ChattadaV@users.noreply.github.com>
2023-04-08 19:04:11 -0500

    Delete CCTV Camera for People with Visual Impairment Disability/CCTV-Camera-Project-master directory

--- CCTV Camera for People with Visual Impairment Disability/CCTV-Camera-Project-master/README.md
+++
@@ -1,2 +0,0 @@
-# CCTV-Camera-Project
-$200 CCTV Camera Project for Mr. Michael Papp from Alabama Department of Rehabilitation Services Fall 2020
--- CCTV Camera for People with Visual Impairment Disability/CCTV-Camera-Project-master/Raspberry-Pi-CCTV-master/Gimp_For_Cropping.py
+++
@@ -1,6 +0,0 @@
-import os
-
-output=os.system('sudo apt -y install gimp')
-output=os.system('sudo apt update')
-output=os.system('sudo apt -y install mirage')
-
